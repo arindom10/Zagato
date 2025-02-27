8e7b8da2b0343e6e49fdec6b62657c87c9cfe694
arindom10 <karindom7@gmail.com>
2025-02-27 16:53:53 +0600

    Add About page component with HomeLayout integration

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import HomeLayout from "@/Layout/HomeLayout";
+import React from "react";
+
+const page = () => {
+  return (
+    <HomeLayout>
+      <div>
+        <h2 className="text-6xl text-red-800">This is about page</h2>
+      </div>
+    </HomeLayout>
+  );
+};
+
+export default page;
