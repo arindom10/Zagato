42428884b824a94d43d22d8f5825139e02c51069
arindom10 <karindom7@gmail.com>
2025-02-28 11:35:53 +0600

    Update HomePageOverview title styling for improved visibility

--- src/Components/HomePageOverview/HomePageOverview.tsx
+++ src/Components/HomePageOverview/HomePageOverview.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePageOverview = () => {
   return (
     <div>
-      <h1>This is HomePageOverview</h1>
+      <h1 className="text-6xl text-red-800">This is HomePageOverview !!</h1>
     </div>
   );
 };
